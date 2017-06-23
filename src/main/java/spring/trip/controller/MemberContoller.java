cb97bf7a480b1137b01605d3552e5cacc4fa8bc2
kosta-HP\kosta <woojong610@gmail.com>
2017-06-23 18:12:13 +0900

    MemberDAO, MemberService, MemberController 메소드 선언(우정)

--- src/main/java/spring/trip/controller/MemberContoller.java
+++ src/main/java/spring/trip/controller/MemberContoller.java
@@ -1,5 +1,57 @@
 package spring.trip.controller;
 
-public class MemberContoller {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class MemberContoller extends MultiActionController {
+	// isEmailExist
+	public ModelAndView isEmailExist(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// isNicknameExist
+	public ModelAndView isNicknameExist(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// registerMember
+	public ModelAndView registerMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// checkPassword
+	public ModelAndView checkPassword(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// removeMember
+	public ModelAndView removeMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// login
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// showMemberInfo
+	public ModelAndView showMemberInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
+
+	// updateMember
+	public ModelAndView updateMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView();
+	}
 }
--- src/main/java/spring/trip/model/service/MemberService.java
+++ src/main/java/spring/trip/model/service/MemberService.java
@@ -1,5 +1,14 @@
 package spring.trip.model.service;
 
-public interface MemberService {
+import spring.trip.domain.Member;
 
+public interface MemberService {
+	boolean isEmailExist(String email);				// 이메일 중복 확인
+	boolean isNicknameExist(String nickname);		// 닉네임 중복 확인
+	int registerMember(Member vo);					// 회원가입
+	boolean checkPassword(String password);			// 비밀번호 맞는지 확인
+	int removeMember(String email);					// 회원탈퇴
+	Member login(String email, String password);	// 로그인
+	Member showMemberInfo(String email);			// 멤버 정보 조회
+	int updateMember(Member vo);					// 멤버 정보 수정
 }

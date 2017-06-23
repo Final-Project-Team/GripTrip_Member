4f6963cfcfa1f2dd4ef18ce6f438c0c3ae8935c2
KOSTA\코스타 <cyber881228@gmail.com>
2017-06-23 19:01:58 +0900

    service 작성 중

--- src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
+++ src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
@@ -1,5 +1,70 @@
 package spring.trip.model.service.impl;
 
-public class MemberServiceImpl {
+import org.apache.ibatis.session.SqlSession;
+
+import spring.trip.domain.Member;
+import spring.trip.model.dao.MemberDAO;
+import spring.trip.model.service.MemberService;
+
+public class MemberServiceImpl implements MemberService{
+
+	private SqlSession sqlSession;
+	private MemberDAO memberDAO;
+	
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	public void setMemberDAO(MemberDAO memberDAO) {
+		this.memberDAO = memberDAO;
+	}
+
+	@Override
+	public boolean isEmailExist(String email) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean isNicknameExist(String nickname) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int registerMember(Member vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean checkPassword(String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int removeMember(String email) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Member login(String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member showMemberInfo(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateMember(Member vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }

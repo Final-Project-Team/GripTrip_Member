c6f69bc584ba91bcb9e77ec0e9dc269361f8ce0d
kosta-HP\kosta <woojong610@gmail.com>
2017-06-23 16:45:15 +0900

    member.xml 쿼리문 선언, MemberDAO 메소드 선언(우정)

--- src/main/java/spring/trip/model/dao/MemberDAO.java
+++ src/main/java/spring/trip/model/dao/MemberDAO.java
@@ -1,5 +1,12 @@
 package spring.trip.model.dao;
 
-public interface MemberDAO {
+import spring.trip.domain.Member;
 
+public interface MemberDAO {
+	boolean isEmailExist(String email);
+	boolean isNicknameExist(String nickname);
+	int registerMember(Member vo);
+	boolean checkPassword(String password);
+	int removeMember(String email);
+	Member login(String email, String password);
 }

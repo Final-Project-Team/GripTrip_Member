ce0fdc45a9fefd445863eff313490141dd13be69
kosta-HP\kosta <annualring4@gmail.com>
2017-06-24 15:57:10 +0900

    memberServiceImpl 작성(안)

--- src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
+++ src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
@@ -7,54 +7,53 @@ import spring.trip.model.dao.MemberDAO;
 import spring.trip.model.service.MemberService;
 
 public class MemberServiceImpl implements MemberService{
-
+	private MemberDAO memberDAO;
+	
 	@Override
 	public boolean isEmailExist(String email) {
-		// TODO Auto-generated method stub
-		return false;
+
+		return memberDAO.isEmailExist(email);
 	}
 
 	@Override
 	public boolean isNicknameExist(String nickname) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return memberDAO.isNicknameExist(nickname);
 	}
 
 	@Override
 	public int registerMember(Member vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return memberDAO.registerMember(vo);
 	}
 
 	@Override
 	public boolean checkPassword(String password) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return memberDAO.checkPassword(password);
 	}
 
 	@Override
 	public int removeMember(String email) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
-	public Member login(Member member) {
-		// TODO Auto-generated method stub
+	public Member login(Member vo) {
+		
 		return null;
 	}
 
 	@Override
 	public Member showMemberInfo(String email) {
-		// TODO Auto-generated method stub
+	
 		return null;
 	}
 
 	@Override
 	public int updateMember(Member vo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	
+		return 0;
+	}	
 }

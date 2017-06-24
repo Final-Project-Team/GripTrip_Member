faa4b4c525d385be52a1a83942b6154a4ebf9a8b
kosta-HP\kosta <annualring4@gmail.com>
2017-06-24 09:04:46 +0900

    DAO/Service Impl 작성(안)

--- src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
+++ src/main/java/spring/trip/model/service/impl/MemberServiceImpl.java
@@ -1,5 +1,62 @@
 package spring.trip.model.service.impl;
 
-public class MemberServiceImpl {
+import spring.trip.domain.Member;
+import spring.trip.model.dao.MemberDAO;
+import spring.trip.model.service.MemberService;
 
+public class MemberServiceImpl implements MemberService{
+	private MemberDAO memberDAO;
+	
+	public void setMemberDAO(MemberDAO memberDAO) {
+		this.memberDAO = memberDAO;
+	}
+
+	@Override
+	public boolean isEmailExist(String email) {
+		memberDAO.isEmailExist(email);
+		return false;
+	}
+
+	@Override
+	public boolean isNicknameExist(String nickname) {
+		memberDAO.isNicknameExist(nickname);
+		return false;
+	}
+
+	@Override
+	public int registerMember(Member vo) {
+		int reg = memberDAO.registerMember(vo);
+		return reg;
+	}
+
+	@Override
+	public boolean checkPassword(String password) {
+		memberDAO.checkPassword(password);
+		return false;
+	}
+
+	@Override
+	public int removeMember(String email) {
+		int rem = memberDAO.removeMember(email);
+		return rem;
+	}
+
+	@Override
+	public Member login(Member member) {
+		Member mb = memberDAO.login(member);
+		return mb;
+	}
+
+	@Override
+	public Member showMemberInfo(String email) {
+		Member mb = memberDAO.showMemberInfo(email);
+		return mb;
+	}
+
+	@Override
+	public int updateMember(Member vo) {
+		int up = memberDAO.updateMember(vo);
+		return up;
+	}
+	
 }
